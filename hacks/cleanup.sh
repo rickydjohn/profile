8abf310b82b8b24cee47867b5cd591d3e8820000
Ricky DJ <rickydjohn@gmail.com>
2023-12-03 20:02:25 +0530

    pushing cleanup

--- hacks/cleanup.sh
+++ hacks/cleanup.sh
@@ -3,22 +3,11 @@
 
 set -x
 
-val="$(kubectl get applications.argoproj.io -n argocd -l expiry,environment=dev -o custom-columns=":.metadata.name,:.metadata.labels.deleteAfter")"
 
-
-while IFS= read -r line; do
-  name=$(echo $line | awk '{print $1}')
-  expiry=$(echo $line | awk '{print $2}')
-  
-  if [[ ${#name} -eq 0 ]]; then
-    continue
+for i in $(kubectl get applications.argoproj.io -n argocd -l expiry,environment=dev -o custom-columns=":.metadata.name"); do
+  expiry=$(kubectl get applications.argoproj.io -n argocd $i -o jsonpath='{.metadata.labels.deleteAfter}')
+  if [[ $(date +%s) -ge $expiry ]]; then
+    echo "deleting argocd application $i with expiry at $expiry"
+    kubectl delete applications.argoproj.io -n argocd $i
   fi
-
-  if [[ `date +%s` -ge $expiry ]]; then
-    echo "deleting app $name expiring at $expiry"
-    kubectl delete -n argocd applications.argoproj.io $name
-    if [[ $? -ne 0 ]]; then
-      exit 1
-    fi
-  fi
-done <<<  "$val"
+done
